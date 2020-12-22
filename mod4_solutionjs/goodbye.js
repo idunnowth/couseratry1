c205fd033346c073a3f65b82ddcbd6996df3cce0
idunnowth <tohyicheng1989@gmail.com>
2020-12-22 12:13:41 +0800

    trial

--- mod4_solutionjs/goodbye.js
+++ mod4_solutionjs/goodbye.js
@@ -25,4 +25,3 @@ window.byeSpeaker = byeSpeaker.speak
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
-//
